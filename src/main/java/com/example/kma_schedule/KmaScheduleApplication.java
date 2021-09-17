7b18d14d8b7f27f0343672d97ae87b46726278f5
yar0slava <yavacollins@gmail.com>
2021-09-17 21:27:50 +0300

    initial commit

---
+++ src/main/java/com/example/kma_schedule/KmaScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.kma_schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmaScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmaScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kma_schedule/KmaScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kma_schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmaScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
