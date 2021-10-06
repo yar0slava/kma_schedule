8f1e69d3a21cea9b603e9344503ca7446fe800a6
irynabolyukh <iryna.boliukh@ukma.edu.ua>
2021-10-06 20:09:12 +0300

    extra removed

--- src/main/java/com/example/kma_schedule/KmaScheduleApplication.java
+++ src/main/java/com/example/kma_schedule/KmaScheduleApplication.java
@@ -1,9 +1,6 @@
 package com.example.kma_schedule;
 
-import com.example.schedulerspringbootstarter.ScheduleServiceImpl;
 import com.example.schedulerspringbootstarter.WebSecurityConfigurer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
