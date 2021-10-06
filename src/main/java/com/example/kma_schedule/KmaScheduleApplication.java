bb3b777c0c411edc1dd0e1c06b9baa2ec459166a
irynabolyukh <iryna.boliukh@ukma.edu.ua>
2021-10-06 17:07:48 +0300

    annotation properties usage added

--- src/main/java/com/example/kma_schedule/KmaScheduleApplication.java
+++ src/main/java/com/example/kma_schedule/KmaScheduleApplication.java
@@ -1,23 +1,13 @@
 package com.example.kma_schedule;
 
-import com.example.kma_schedule.database.entity.Record;
-import com.example.kma_schedule.database.repository.RecordRepository;
-import com.example.schedulerspringbootstarter.ScheduleService;
 import com.example.schedulerspringbootstarter.ScheduleServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class KmaScheduleApplication {
-
 
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = SpringApplication.run(KmaScheduleApplication.class, args);
@@ -25,6 +15,5 @@ public class KmaScheduleApplication {
 		ScheduleServiceImpl scheduleService = applicationContext.getBean(ScheduleServiceImpl.class);
 		scheduleService.doSmth();
 	}
-
 
 }
