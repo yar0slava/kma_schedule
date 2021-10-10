7c503fe3abb10e9d2685d82c7d1d16bacb98478b
Maksym Pidlisnyi <p.maxsym.w@gmail.com>
2021-10-10 21:55:38 +0300

    add dev and prod configurations + h2 database

--- src/main/java/com/example/kma_schedule/KmaScheduleApplication.java
+++ src/main/java/com/example/kma_schedule/KmaScheduleApplication.java
@@ -1,27 +1,26 @@
 package com.example.kma_schedule;
 
-import com.example.schedulerspringbootstarter.WebSecurityConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class KmaScheduleApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = SpringApplication.run(KmaScheduleApplication.class, args);
+		SpringApplication.run(KmaScheduleApplication.class, args);
+//		ApplicationContext applicationContext = SpringApplication.run(KmaScheduleApplication.class, args);
 
-		WebSecurityConfigurer webSecurityConfigurer = applicationContext.getBean(WebSecurityConfigurer.class);
-		String[] adminEnd = webSecurityConfigurer.getAdminEndpoints();
-		for (String s: adminEnd) {
-			System.out.println(s);
-		}
-		System.out.println("===========");
-		String[] unsecured = webSecurityConfigurer.getUnsecuredEndpoints();
-		for (String s: unsecured) {
-			System.out.println(s);
-		}
+//		WebSecurityConfigurer webSecurityConfigurer = applicationContext.getBean(WebSecurityConfigurer.class);
+//		String[] adminEnd = webSecurityConfigurer.getAdminEndpoints();
+//		for (String s: adminEnd) {
+//			System.out.println(s);
+//		}
+//		System.out.println("===========");
+//		String[] unsecured = webSecurityConfigurer.getUnsecuredEndpoints();
+//		for (String s: unsecured) {
+//			System.out.println(s);
+//		}
 	}
 
 }
