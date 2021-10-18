9aca921893787914eb37bcf35b33aa26b3a320b4
Maksym Pidlisnyi <p.maxsym.w@gmail.com>
2021-10-18 23:06:13 +0300

    add log4j2

--- src/main/java/com/example/kma_schedule/KmaScheduleApplication.java
+++ src/main/java/com/example/kma_schedule/KmaScheduleApplication.java
@@ -1,14 +1,20 @@
 package com.example.kma_schedule;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-public class KmaScheduleApplication {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class KmaScheduleApplication implements ApplicationRunner {
+	private static final Logger logger = LogManager.getLogger(KmaScheduleApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(KmaScheduleApplication.class, args);
+
 //		ApplicationContext applicationContext = SpringApplication.run(KmaScheduleApplication.class, args);
 
 //		WebSecurityConfigurer webSecurityConfigurer = applicationContext.getBean(WebSecurityConfigurer.class);
@@ -21,6 +27,15 @@ public class KmaScheduleApplication {
 //		for (String s: unsecured) {
 //			System.out.println(s);
 //		}
+	}
+
+	@Override
+	public void run(ApplicationArguments applicationArguments) {
+		logger.debug("Debugging log");
+		logger.info("Info log");
+		logger.warn("Hey, This is a warning!");
+		logger.error("Oops! We have an Error. OK");
+		logger.fatal("Damn! Fatal error. Please fix me.");
 	}
 
 }
