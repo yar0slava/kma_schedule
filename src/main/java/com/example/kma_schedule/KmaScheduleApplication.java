4490e9db407e45607c5c0a794d21b167e2c51b44
Maksym Pidlisnyi <p.maxsym.w@gmail.com>
2021-10-18 23:38:24 +0300

    add admin logger marker

--- src/main/java/com/example/kma_schedule/KmaScheduleApplication.java
+++ src/main/java/com/example/kma_schedule/KmaScheduleApplication.java
@@ -2,6 +2,8 @@ package com.example.kma_schedule;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,6 +13,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class KmaScheduleApplication implements ApplicationRunner {
 	private static final Logger logger = LogManager.getLogger(KmaScheduleApplication.class);
+	private static final Marker marker = MarkerManager.getMarker("ADMIN");
 
 	public static void main(String[] args) {
 		SpringApplication.run(KmaScheduleApplication.class, args);
@@ -36,6 +39,9 @@ public class KmaScheduleApplication implements ApplicationRunner {
 		logger.warn("Hey, This is a warning!");
 		logger.error("Oops! We have an Error. OK");
 		logger.fatal("Damn! Fatal error. Please fix me.");
+
+		logger.info(marker, "ADMIN info");
+		logger.error(marker, "ADMIN Error");
 	}
 
 }
