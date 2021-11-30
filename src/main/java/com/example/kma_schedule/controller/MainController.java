f897855b8258ce4ed6f8a5dde75d45588d3ea5b9
irynabolyukh <iryna.boliukh@ukma.edu.ua>
2021-11-30 11:08:51 +0200

    open resource

--- src/main/java/com/example/kma_schedule/controller/MainController.java
+++ src/main/java/com/example/kma_schedule/controller/MainController.java
@@ -1,12 +1,35 @@
 package com.example.kma_schedule.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 public class MainController {
+
+    @RequestMapping(value = "/data/book.pdf", method = RequestMethod.GET)
+    public void getSteamingFile1(HttpServletResponse response) throws IOException {
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "attachment; filename=\"book.pdf\"");
+        InputStream inputStream = new FileInputStream(new File("C:\\Users\\iryna\\kma_schedule\\src\\main\\resources\\data\\book.pdf"));
+        int nRead;
+        while ((nRead = inputStream.read()) != -1) {
+            response.getWriter().write(nRead);
+        }
+    }
+
     @GetMapping("/")
     @ResponseStatus(HttpStatus.OK)
     public String getMain(){
