3a335de984b40a3a94304d572a0460949df58757
yar0slava <yavacollins@gmail.com>
2021-12-01 23:15:16 +0200

    added aspects for logging method parameters and method execution time

---
+++ src/main/java/com/example/kma_schedule/aop/TimeLoggingAspect.java
@@ -0,0 +1,23 @@
+package com.example.kma_schedule.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeLoggingAspect {
+
+    @Around("@annotation(LogExecutionTime)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+
+        Object proceed = joinPoint.proceed();
+
+        long executionTime = System.currentTimeMillis() - start;
+
+        System.out.println(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+        return proceed;
+    }
+}
