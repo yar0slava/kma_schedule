086a5fc6f72902da234f87623fdf1369059e526c
Maksym Pidlisnyi <p.maxsym.w@gmail.com>
2021-12-01 21:25:27 +0200

    add mdc from practice

--- src/main/java/com/example/kma_schedule/controller/UserController.java
+++ src/main/java/com/example/kma_schedule/controller/UserController.java
@@ -5,16 +5,16 @@ import com.example.kma_schedule.database.entity.User;
 import com.example.kma_schedule.dto.LoginDto;
 import com.example.kma_schedule.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 public class UserController {
 
     private final UserServiceImpl userService;
--- src/test/java/com/example/kma_schedule/SpringWebTest.java
+++ src/test/java/com/example/kma_schedule/SpringWebTest.java
@@ -1,6 +1,7 @@
 package com.example.kma_schedule;
 
 import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,20 +9,32 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+
+//@WebMvcTest(UserController.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class SpringWebTest {
 
     @Autowired
+    private WebApplicationContext webApplicationContext;
+
     private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
 
     @Test
     @SneakyThrows
-    public void openSignInPageTest() {
+    public void givenSignInURI_whenMockMVC_thenStatusIsOk() {
         mockMvc
                 .perform(MockMvcRequestBuilders.get("/sign-in"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
 }
+
